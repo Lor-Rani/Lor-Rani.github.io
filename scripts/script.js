a298db2659d1419f904134cf73b8bf48d4a1dbe3
Lor-Rani <43768295+Lor-Rani@users.noreply.github.com>
2020-10-23 21:50:55 +1000

    player pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,22 @@
-function showButton() {
-    var prompts = document.getElementById("prompts");
-    var answers = document.getElementById("q1");
-    if (prompts.style.display === "none") {
-        prompts.style.display = "block";
-    }
-    if (answers.style.display === "none") {
-        answers.style.display = "block";
-    }
+var questionNo = 1;
+   
+$(document).ready(function() {
+    questionNo = 1;
+    $("#next").click(function() {
+        goToByScroll("#answers");
+    });
+
+    $("#nextq").click(function() {
+        goToByScroll("#questions");
+        $(".q" + questionNo).css("display", "none");
+        $(".q" + (questionNo + 1)).css("display", "block");
+        questionNo += 1;
+    });
+});
+
+function goToByScroll(id) {
+    // Scroll
+    $('html,body').animate({
+        scrollTop: $(id).offset().top
+    }, 'slow');
 }
